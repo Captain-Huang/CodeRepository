f431da45361f1cb5a807676ea9637567ab724d80
tongshuaihuang <503159545@qq.com>
2020-08-06 21:25:53 +0800

    添加算法第四版代码练习项目

---
+++ VisualStudio/ALG_4_Code/ALG_4_Code/Program.cs
@@ -0,0 +1,19 @@
+﻿using framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALG_4_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogHelper.info("Hello ALG");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ VisualStudio/ALG_4_Code/ALG_4_Code/src/framework/LogHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace framework
+{
+    class LogHelper
+    {
+        public static void info(object msg)
+        {
+            string _msg = (string)msg;
+            Console.WriteLine(msg);
+        }
+    }
+}
